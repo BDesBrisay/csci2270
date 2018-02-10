e4ef46873edd44e38cd2eda59565658b98db81a3
BDesBrisay <bryce@desbrisay.com>
2018-02-09 17:02:17 -0700

    finished assignment 3

--- assignment3/network.cpp
+++ assignment3/network.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
 
 struct city {
-    string name;
+    string cityName;
     string message;
     city* next;
     city() {}
     city(string n, string m) {
-        name = n;
+        cityName = n;
         message = m;
     }
     city(string n, string m, city* ne) {
-        name = n;
+        cityName = n;
         message = m;
         next = ne;
     }
@@ -30,7 +32,7 @@ class network {
 
         void add(string name) {
             city* temp = new city; 
-            temp->name = name;
+            temp->cityName = name;
             temp->next = NULL;
 
             if (head == NULL) {
@@ -40,19 +42,61 @@ class network {
                 tail->next = temp;
                 tail = tail->next;
             }
-        }
-
-        
+        }   
 };
 
-void print(city* head) {
+city* addCity(city* head, city* prev, string newName) {
+    city* temp = new city;
+    temp->cityName = newName;
+    temp->next = NULL;
+    city* place = head;
+    
+    if (head == NULL) {
+        head = temp;
+    } else if (prev == NULL) {
+        temp->next = head;
+        head = temp;
+    } else {
+        while (place != NULL) {
+            if (place->cityName == prev->cityName) {
+                temp->next = place->next;
+                place->next = temp;
+                break;
+            } else {
+                place = place->next;
+            }
+        }
+    }
+    
+    return head;
+}
+
+void transmitMsg(city* head) {
+    ifstream file("messageIn.txt");
+    string line;
+    city* place = head;
+    
+    if (file.is_open()) {
+        while (getline(file, line, ' ')) {
+            while (place != NULL) {
+                line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+                place->message = line;
+                cout << place->cityName << " received " << place->message << endl;
+                place = place->next;
+            }
+            place = head;
+        }
+    }
+}
+
+void printPath(city* head) {
     city *place = head;
     cout << "===CURRENT PATH===" << endl;
     while ( place != NULL ) {
-        cout << place->name << " -> ";
+        cout << place->cityName << " -> ";
         place = place->next;
     }
-    cout << "NULL" << endl;
+    cout << " NULL" << endl;
     cout << "==================" << endl;
 }
 
@@ -64,13 +108,15 @@ city* buildNetwork() {
         a.add(names[i]);
     }
 
-    print(a.head);
+    // printPath(a.head);
     return a.head;
 }
 
 void displayMenu() {
     int valid = 0;
     string input;
+    city* head = buildNetwork();
+
     while (!valid) {
         cout << "======Main Menu======" << endl;
         cout << "1. Build Network" << endl;	
@@ -84,20 +130,25 @@ void displayMenu() {
             int selection = stoi(input);
             if (selection >= 1 && selection <= 5) {
                 if (selection == 1) {
-                    city* head = buildNetwork();
+                    city* newhead = buildNetwork();
+                    // printPath(newhead);
                 } else if (selection == 2) {
+                    printPath(head);
+                } else if (selection == 3) {
+                    transmitMsg(head);
+                } else if (selection == 4) {
                     string newCity;
                     string prevCity;
-                    cout << "Enter a city name: " << endl;
+                    cout << "Enter a City name: " << endl;
                     cin >> newCity;
-                    cout << "Enter a previous city name: " << endl;
+                    cout << "Enter a previous City name: " << endl;
                     cin >> prevCity;
-                    cout << newCity << prevCity << endl;
+                    
+                    city* prev = new city;
+                    prev->cityName = prevCity;
 
-                } else if (selection == 3) {
-
-                } else if (selection == 4) {
-
+                    city* newhead = addCity(head, prev, newCity);
+                    // printPath(newhead);
                 } else if (selection == 5) {
                     cout << "Goodbye!" << endl;
                     valid = 1;
