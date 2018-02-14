fb3989f39955c4dd522a7eadf22119d7bafd0ee0
BDesBrisay <bryce@desbrisay.com>
2018-02-13 17:44:04 -0700

    finished rec4

--- rec4/doublyLL.cpp
+++ rec4/doublyLL.cpp
@@ -19,11 +19,31 @@ bool doublyLL::insertNodeAtEnd(int a) {
 }
 
 bool doublyLL::deleteNode(int a) {
+    Node* place = head;
 
+    while (place != nullptr) {
+        if (place->key == a && place->next != nullptr && place->prev != nullptr) {
+            place->prev->next = place->next;
+            place->next->prev = place->prev;
+            place = place->next;
+        } else if (place->key == a && place->next == nullptr) {
+            place->prev->next = nullptr;
+            tail = place->prev;
+            place = nullptr;
+        } else if (place->key == a && place->prev == nullptr) {
+            place->next->prev = nullptr;
+            place = place->next;
+            head = place;
+        } else {
+            place = place->next;
+        }
+    }
 }
 
 void doublyLL::printList() {
     Node* place = head;
+
+    cout << "Head and Tail: " << head->key << ", " << tail->key << endl;
 
     while (place != nullptr) {
         cout << place->key << endl;
@@ -32,11 +52,16 @@ void doublyLL::printList() {
 }
 
 doublyLL::doublyLL() {
-
+    head = nullptr;
+    tail = nullptr;
 }
 
-doublyLL::doublyLL(int) {
- 
+doublyLL::doublyLL(int a) {
+    Node* temp = new Node;
+    temp->key = a;
+    temp->prev = nullptr;
+    temp->next = nullptr;
+    head = temp;
 }
 
 doublyLL::~doublyLL() {
--- rec4/main.cpp
+++ rec4/main.cpp
@@ -7,5 +7,36 @@ int main() {
     doublyLL list;
 
     list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(1);
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(2);
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(3);
+   
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(5);
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(6);
+    list.insertNodeAtEnd(4);
+    
+    list.insertNodeAtEnd(7);
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(8);
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(9);
+    
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(10);
+    list.insertNodeAtEnd(4);
+    list.insertNodeAtEnd(11);
+    list.insertNodeAtEnd(4);
+    
+    cout << "Initial List:" << endl;
+    list.printList();
+
+    list.deleteNode(4);
+
+    cout << endl;
+    cout << "Updated List:" << endl;
     list.printList();
 }
